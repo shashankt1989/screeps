155f4f642d36a970008bcc6d8a58e6ede5d8f2f6
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-12 03:11:03 -0800

    tower repair

--- src/logic.tower.js
+++ src/logic.tower.js
@@ -3,13 +3,22 @@ var logicTower = {
     run: function(tower,currRoom) {
 
         // If any invader present then attack them
-        var target = currRoom.find(FIND_HOSTILE_CREEPS);
-        if (target.length > 0)
+        var targets = currRoom.find(FIND_HOSTILE_CREEPS);
+        if (targets.length > 0)
         {
-            tower.attack(target[0]);
+            tower.attack(targets[0]);
         }
         else
         {
+            targets = currRoom.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_WALL) && structure.hits < (structure.hitsMax /2) && structure.hits < 25000
+                }
+            });
+            
+            if(targets.length > 0) {
+                tower.repair(targets[0]);
+            }
 
         }
    }
