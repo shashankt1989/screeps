1a3a1193613e9faa10a06978e45857c43237ae51
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-12 05:06:26 -0800

    harvester storage update

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -15,7 +15,8 @@ var roleHarvester = {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_SPAWN ||
                         structure.structureType == STRUCTURE_TOWER ||
-                        structure.structureType == STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;
+                        structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_STORAGE) && structure.energy < structure.energyCapacity;
                 }
             });
             if(targets.length > 0) {
