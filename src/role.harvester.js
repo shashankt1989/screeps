4b510f770c5e2a5f2707a6fbf2ce6000d187be38
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-12 04:52:25 -0800

    harvester bigger bug

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -2,13 +2,15 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy == 0) {
+        if(creep.carry.energy == 0 || (creep.memory.harvesting == true && creep.carry.energy < creep.energy.carryCapacity)) {
+            creep.memory.harvesting = true;
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ff0000'}});
             }
         }
         else {
+            creep.memory.harvesting = false;
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_SPAWN ||
