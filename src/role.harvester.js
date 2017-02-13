769df747b429e5fde7b4b0d9ca973ba9381329b2
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-12 20:49:59 -0800

    harvester bug

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -24,7 +24,7 @@ var roleHarvester = {
             }
             else
             {
-                var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {structure.structureType == STRUCTURE_STORAGE}});
+                var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return structure.structureType == STRUCTURE_STORAGE}});
                 if(targets.length > 0) {
                     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#aa0000'}});
