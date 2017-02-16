9ddb77babdbc2a8e07c5d148dec1dc0d5f27b0d0
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-16 15:23:48 -0800

    link logic bug

--- src/logic.link.js
+++ src/logic.link.js
@@ -4,10 +4,10 @@ var logicLink = {
         var range = 10;
 
         var targets = link.pos.findInRange(FIND_STRUCTURES, range, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_STORAGE && structure.store[RESOURCE_ENERGY] < structure.storeCapacity)
-                }
-            );
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_STORAGE && structure.store[RESOURCE_ENERGY] < structure.storeCapacity)
+            }
+        });
         if (targets.length > 0)
         {
             link.transferEnergy(targets[0]);
@@ -18,7 +18,7 @@ var logicLink = {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_LINK && structure.energy < structure.energyCapacity)
                 }
-            );
+            });
             if(target)
             {
                 link.transferEnergy(target);
