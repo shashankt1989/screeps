a3cc17255ab09cee9e4ea7807186115f547cf24d
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-15 19:16:34 -0800

    turn on creep dying logic with fix

--- src/spawn.utility.js
+++ src/spawn.utility.js
@@ -128,7 +128,7 @@ var spawnUtility = {
             return false;
 
         // if creep is about to die off then consider that creep as good as dead! 
-        var currCreeps = _.filter(Game.creeps, (creep) => creep.memory.role == role && creep.memory.targetRoom == roomName);
+        var currCreeps = _.filter(Game.creeps, (creep) => creep.memory.role == role && creep.memory.targetRoom == roomName && (creep.spawning || creep.ticksToLive > 100) );
         var currCount = currCreeps.length;
         if(currCount >= maxCount)
             return false;
