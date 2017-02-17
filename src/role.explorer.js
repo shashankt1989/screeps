dd06c654e8995340c1f62d39977377fa27c0c4b8
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-16 17:25:36 -0800

    explorer drop all resources and return after collecting anything

--- src/role.explorer.js
+++ src/role.explorer.js
@@ -3,7 +3,7 @@ var roleExplorer = {
     run: function(creep) {
         var targetRoom = creep.memory.targetRoom;
         var spawnRoom = Game.rooms[creep.memory.sourceRoom];
-        if(creep.carry.energy < creep.carryCapacity) {
+        if(creep.carry.energy == 0) {
             if(creep.room.name != targetRoom)
             {
                 var exitDir = Game.map.findExit(creep.room, targetRoom);
@@ -38,7 +38,7 @@ var roleExplorer = {
             }
             else
             {
-                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                var target = creep.pos.findClosestByRange(FIND_STRUCTURES,{
                     filter: (structure) => {
                         return structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_LINK;
                     }
