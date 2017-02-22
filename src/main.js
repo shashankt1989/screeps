ec750512a4c322463ff4af1adbf7fcaa6f8ce321
Shashank Tyagi <shashank.t1989@outlook.com>
2017-02-22 02:00:39 -0800

    delete unused room

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ module.exports.loop = function () {
         var currSpawn = Game.spawns[spawnName]; 
         var currRoom = currSpawn.room;
 
-        var rooms = [currRoom.name, "W82N9", "W81N8"];
+        var rooms = [currRoom.name, "W82N9"];
 
         var towers = currRoom.find(FIND_STRUCTURES, {
                     filter: (structure) => {
