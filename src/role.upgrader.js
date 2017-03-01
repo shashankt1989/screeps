b28f01c9c0a37909f8d9c168609bed9aefdc7d29
Shashank Tyagi <shashank.t1989@outlook.com>
2017-03-01 00:12:17 -0800

    upgrader source selection bug

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -31,14 +31,14 @@ var roleUpgrader = {
             else {
                 creep.memory.mining = true;
                 // find a storage with non zero energy
-                var targets = creep.room.find(FIND_STRUCTURES, {
+                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                         filter: (structure) => {
                             return (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0;
                         }
                     });
-                if(targets.length > 0) {
-                    if(creep.withdraw(targets[0],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
+                if(target) {
+                    if(creep.withdraw(target,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
                     }
                 }
                 else {
